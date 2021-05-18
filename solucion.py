468d274898648cd0265d25cd444192fc346402e7
Danielfmv <44736460+Danielfmv@users.noreply.github.com>
2021-05-17 23:55:00 -0500

    Update solucion.py

--- solucion.py
+++ solucion.py
@@ -13,21 +13,21 @@ def nomina(informacion:dict) -> dict:
     dineroextra = salarioBruto - 1500.00
     
     if salarioBruto > 1500.00 and dineroextra <= 500.00:
-        impuestos = round(((dineroextra * 20)/100),2)
-    elif salarioBruto > 1500.00 and dineroextra > 500.00:
-        impuestos = (((salarioBruto-2000)*30)/100) + ((500)*20)/100
+        impuestos = ((dineroextra * 20)/100)
+    elif salarioBruto > 2000.00 and dineroextra > 500.00:
+        impuestos = (((salarioBruto-2000)*30)/100) + (((500.00)*20)/100)
     else:
         impuestos = 0
+    
     
     salarioNeto = round((salarioBruto - impuestos),2)
 
     diccionarioSalida = dict()
     diccionarioSalida ["id"] = id
     diccionarioSalida ["salarioBruto"] = salarioBruto
-    diccionarioSalida ["impuestos"] = impuestos
+    diccionarioSalida ["impuestos"] = round(impuestos,2)
     diccionarioSalida ["salarioNeto"] = salarioNeto
 
     return diccionarioSalida
 
-informacion1 = {"id":1, "tarifa":50.5, "numeroHoras": 40}
-print(nomina(informacion1))
+informacion1 = {"id":1, "tarifa":40, "numeroHoras": 40}
